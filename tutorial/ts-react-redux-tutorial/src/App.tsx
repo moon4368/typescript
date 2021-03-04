2fbe7bee6016f847414bf8b385cc9fb95b5718fd
moon4368 <inv1064@gmail.com>
2021-03-04 22:07:48 +0900

    typescript react에서 redux 활용하기

---
+++ tutorial/ts-react-redux-tutorial/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <>
+      <TodoInsert />
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
